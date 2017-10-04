4cdc309fc77ae6d669b3c961be3baa6449522bcb
Rachell Calhoun <rachell.calhoun@gmail.com>
2017-10-04 16:14:19 -0400

    fonts static

--- centurion/settings.py
+++ centurion/settings.py
@@ -171,6 +171,3 @@ MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads')
 
 # Extra places for collectstatic to find static files.
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
