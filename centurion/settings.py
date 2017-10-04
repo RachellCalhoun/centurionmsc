9e0cb6921cf353516f8dd662f88a3bd93e6e7ff3
Rachell Calhoun <rachell.calhoun@gmail.com>
2017-10-04 13:34:08 -0400

    settings

--- centurion/settings.py
+++ centurion/settings.py
@@ -11,35 +11,32 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
-
 
 
 DEPLOY = os.getenv('DEPLOY') != 'HEROKU'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = os.getenv('DJANGO_DEBUG') != 'FALSE'
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
     SECRET_KEY = '$dn+%59yjby20dr8e#(&cl4=38i_$-@hp8bm)x=-ln9i^-is89'
+     ALLOWED_HOSTS = ['*']
 else:
     SECRET_KEY = os.getenv('SECRET_KEY')
+    ALLOWED_HOSTS = ['centurionmsc.pythonanywhere.com', 'centurionmsc.com', '.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-if DEBUG:
-    ALLOWED_HOSTS = ['*']
-else:
-    ALLOWED_HOSTS = ['centurionmsc.pythonanywhere.com', 'centurionmsc.com', '.herokuapp.com']
+   
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SITE_ID = 0
+SITE_ID = 1
 
 # Application definition
 
@@ -89,7 +86,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'centurion.wsgi.application'
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -131,42 +133,39 @@ USE_TZ = True
 if DEPLOY:
     import dj_database_url
 
-    ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+    ALLOWED_HOSTS = ['*']
 
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'centurionmsc',
-            'USER': 'name',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
-
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #         'NAME': 'centurionmsc',
+    #         'USER': 'name',
+    #         'PASSWORD': '',
+    #         'HOST': 'localhost',
+    #         'PORT': '',
+    #     }
+    # }
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
-else:
-
-    # Database
-    # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-    
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': "table,xhtmlxtras,spellchecker,paste,searchreplace, save, advhr, advimage, advlink, emotions, iespell, insertdatetime, preview",
