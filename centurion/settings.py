5ac0108e8b57a00309e23dd838fddbd2b631ca93
Rachell Calhoun <rachell.calhoun@gmail.com>
2017-10-04 15:43:08 -0400

    fonts static

--- centurion/settings.py
+++ centurion/settings.py
@@ -166,10 +166,10 @@ TINYMCE_DEFAULT_CONFIG = {
 TINYMCE_SPELLCHECKER = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads')
 
 # Extra places for collectstatic to find static files.
 
